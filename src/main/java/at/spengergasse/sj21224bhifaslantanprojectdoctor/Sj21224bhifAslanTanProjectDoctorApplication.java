7311a7f4c19f666871cd52ceab435593e32ee61b
Sel Tan <TAN19731@spengergasse.at>
2022-04-19 12:02:03 +0200

    Changed input Application class

--- src/main/java/at/spengergasse/sj21224bhifaslantanprojectdoctor/Sj21224bhifAslanTanProjectDoctorApplication.java
+++ src/main/java/at/spengergasse/sj21224bhifaslantanprojectdoctor/Sj21224bhifAslanTanProjectDoctorApplication.java
@@ -21,16 +21,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
+@Controller
 @SpringBootApplication
 public class Sj21224bhifAslanTanProjectDoctorApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Sj21224bhifAslanTanProjectDoctorApplication.class, args);
