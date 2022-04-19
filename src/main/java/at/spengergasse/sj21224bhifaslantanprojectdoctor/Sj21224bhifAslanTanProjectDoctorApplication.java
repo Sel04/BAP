177ace095bef3fdb5fc7f72dc263e71eaf5525c3
Sel Tan <TAN19731@spengergasse.at>
2022-04-19 12:12:39 +0200

    Deleted Responsebody annotation

--- src/main/java/at/spengergasse/sj21224bhifaslantanprojectdoctor/Sj21224bhifAslanTanProjectDoctorApplication.java
+++ src/main/java/at/spengergasse/sj21224bhifaslantanprojectdoctor/Sj21224bhifAslanTanProjectDoctorApplication.java
@@ -1,25 +1,9 @@
 package at.spengergasse.sj21224bhifaslantanprojectdoctor;
 
-import at.spengergasse.sj21224bhifaslantanprojectdoctor.domain.Address;
-import at.spengergasse.sj21224bhifaslantanprojectdoctor.domain.Ordination;
-import at.spengergasse.sj21224bhifaslantanprojectdoctor.persistence.OrdinationRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @Controller
 @SpringBootApplication
