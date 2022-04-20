5331368a57aaf2a7b7b8806fa0936da9f9aa1de6
Sel Tan <TAN19731@spengergasse.at>
2022-04-20 17:42:24 +0200

    Changed port for apis

--- src/main/java/at/spengergasse/sj21224bhifaslantanprojectdoctor/Sj21224bhifAslanTanProjectDoctorApplication.java
+++ src/main/java/at/spengergasse/sj21224bhifaslantanprojectdoctor/Sj21224bhifAslanTanProjectDoctorApplication.java
@@ -18,54 +18,13 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
-@Controller
 @SpringBootApplication
 public class Sj21224bhifAslanTanProjectDoctorApplication {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Sj21224bhifAslanTanProjectDoctorApplication.class, args);
     }
 
-    @RequestMapping("/db")
-    String db(Map<String, Object> model) {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-            ArrayList<String> output = new ArrayList<String>();
-            while (rs.next()) {
-                output.add("Read from DB: " + rs.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "db";
-        } catch (Exception e) {
-            model.put("message", e.getMessage());
-            return "error";
-        }
-    }
-
-    public DataSource dataSource() throws SQLException {
-        if (dbUrl == null || dbUrl.isEmpty()) {
-            return new HikariDataSource();
-        } else {
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl(dbUrl);
-            return new HikariDataSource(config);
-        }
-    }
 
 }
